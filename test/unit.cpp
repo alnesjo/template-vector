b93628b7f0d5601d3c03964cf07a53d7a5a705cb
Robert Alnesjö <alnesjo@kth.se>
2016-11-21 19:21:14 +0100

    More tests

--- test/unit.cpp
+++ test/unit.cpp
@@ -1,38 +1,66 @@
 #include "gtest/gtest.h"
 #include "vector.hpp"
 
-namespace {
-  class VectorTest : public ::testing::Test {
-  protected:
-    VectorTest() {
-    }
-    virtual ~VectorTest() {
-    }
-    virtual void SetUp() {
-    }
-    virtual void TearDown() {
-    }
-  };
-
-  TEST_F(VectorTest, PushBack) {
-    alnesjo::Vector<int> v;
-    for (int i = 0; i < 10; i++) {
-      v.push_back(i);
-    }
-    EXPECT_EQ(9, v[9]);
-    EXPECT_EQ(4, v[4]);
+TEST(VectorTest, PushBack) {
+  alnesjo::Vector<int> v;
+  for (int i = 0; i < 10; i++) {
+    v.push_back(i);
   }
-  TEST_F(VectorTest, InsertAtFront) {
-    alnesjo::Vector<int> v;
-    for (int i = 0; i < 10; i++) {
-      v.insert(0,i);
-    }
-    EXPECT_EQ(0, v[9]);
-    EXPECT_EQ(5, v[4]);
+  EXPECT_EQ(9, v[9]);
+  EXPECT_EQ(4, v[4]);
+}
+
+TEST(VectorTest, SizeConstructor) {
+  alnesjo::Vector<int> v(10);
+  for (int i = 0; i < 10; i++) {
+    EXPECT_EQ(0, v[i]);
   }
 }
 
-int main(int argc, char **argv) {
-  ::testing::InitGoogleTest(&argc, argv);
-  return RUN_ALL_TESTS();
+TEST(VectorTest, CopyConstructor) {
+  alnesjo::Vector<int> u(10,7), v;
+  auto it = u.begin();
+  v = u;
+  EXPECT_NE(it, v.begin());
+  for (int i = 0; i < 10; i++) {
+    EXPECT_EQ(v[i], u[i]);
+  }
+  EXPECT_EQ(v.size(), u.size());
+  v.reset();
+  v.clear();
+  for (int i = 0; i < 10; i++) {
+    EXPECT_EQ(7, u[i]);
+  }
+}
+
+TEST(VectorTest, MoveConstructor) {
+  alnesjo::Vector<int> u(10,7), v;
+  auto it = u.begin();
+  v = std::move(u);
+  EXPECT_EQ(it, v.begin());
+}
+
+TEST(VectorTest, InitializerListConstructor) {
+  alnesjo::Vector<char const *> v({"Hej!", "Jag vill", "ingen", "illa."});
+  EXPECT_STREQ("Hej!", v[0]);
+  EXPECT_STREQ("illa.", v[3]);
+}
+
+
+TEST(VectorTest, InsertAtFront) {
+  alnesjo::Vector<int> v;
+  for (int i = 0; i < 10; i++) {
+    v.insert(0,i);
+  }
+  EXPECT_EQ(0, v[9]);
+  EXPECT_EQ(5, v[4]);
+}
+
+TEST(VectorTest, Clear) {
+  alnesjo::Vector<int> v;
+  for (int i = 0; i < 10; i++) {
+    v.insert(0,i);
+  }
+  EXPECT_EQ(0, v[9]);
+  EXPECT_EQ(5, v[4]);
 }
