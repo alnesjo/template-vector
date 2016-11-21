be9a1ac6b7399e690163b2f5cec0c175a91081e6
Robert Alnesjö <alnesjo@hotmail.com>
2016-11-21 17:00:58 +0100

    Update test.cpp

--- test/test.cpp
+++ test/test.cpp
@@ -1,81 +1,67 @@
-#include "vector.hpp"
-#include <assert.h>
 #include <iostream>
+#include <assert.h>
+#include "vector.hpp"
 
 using std::cout;
 using std::endl;
+using std::out_of_range;
 using alnesjo::vector;
 
+vector<int> v;
+
 int main() {
-  vector<int> v({1,2,3,4});
-  for (auto & elem : v) {
-    cout << elem << " ";
+  {
+    v = vector({1,2,3,4});
+    assert(v[0]==1);
+    assert(v[1]==2);
+    assert(v[2]==3);
+    assert(v[3]==4);
+    cout << "OK: il-constructor" << endl;
   }
-  cout << endl;
-  assert(v[0]==1);
-  assert(v[1]==2);
-  assert(v[2]==3);
-  assert(v[3]==4);
-  cout << "OK: il-constructor" << endl;
-
-  auto rit = v.rbegin() - 2;
-  cout << *rit << endl;
-  assert(*rit==2);
-  cout << "OK: reverse iterator" << endl;
-
+  {
+    auto rit = v.rbegin() - 2;
+    assert(*rit==2);
+    cout << "OK: reverse iterator" << endl;
+  }
   {
     bool caught = false;
     try {
       v.insert(6, 1);
-    } catch (std::out_of_range e) {
+    } catch (out_of_range e) {
       caught = true;
-      cout << e.what() << endl;
     }
     assert(caught);
     cout << "OK: out_of_range" << endl;
   }
-
-  v.insert(0, 1);
-  assert(v[0]==1);
-  v.insert(0, 2);
-  assert(v[1]==1);
-  v.insert(0, 3);
-  assert(v[1]==2);
-  v.insert(0, 4);
-  assert(v[1]==3);
-  for (auto & elem : v) {
-    cout << elem << " ";
+  {
+    v.insert(0, 1);
+    assert(v[0]==1);
+    v.insert(0, 2);
+    assert(v[1]==1);
+    v.insert(0, 3);
+    assert(v[1]==2);
+    v.insert(0, 4);
+    assert(v[1]==3);
+    cout << "OK: insert" << endl;
   }
-  cout << endl;
-  cout << "OK: insert" << endl;
-
-  v.erase(3);
-  v.erase(2);
-  v.erase(1);
-  v.erase(0);
-  for (auto & elem : v) {
-    cout << elem << " ";
+  {
+    v.erase(3);
+    v.erase(2);
+    v.erase(1);
+    v.erase(0);
+    cout << "OK: erase" << endl;
   }
-  cout << endl;
-  cout << "OK: erase" << endl;
   {
     vector<int> u = v;
-    for (auto & elem : u) {
-      cout << elem << " ";
-    }
-    cout << endl;
     assert(v[0]==u[0]);
     assert(v[1]==u[1]);
     assert(v[2]==u[2]);
     assert(v[3]==u[3]);
     cout << "OK: copy" << endl;
   }
-
   {
     auto f1 = v.find(1);
-    cout << "find(1) " << f1 << endl;
     auto f5 = v.find(5);
-    cout << "find(5) " << f5 << endl;
     assert(f1==v.begin());
     assert(f5==v.end());
     cout << "OK: find" << endl;
