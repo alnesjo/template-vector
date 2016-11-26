562ccdaf6ab8bb5fdd31a75bbebd7525e93ae014
Robert Alnesjö <alnesjo@kth.se>
2016-11-26 09:29:22 +0100

    New macro EXPECT_ERROR

--- test/vector.cpp
+++ test/vector.cpp
@@ -1,7 +1,17 @@
 #include "gtest/gtest.h"
 #include "vector.hpp"
 
-TEST(VectorTest, InitializerListConstructor) {
+#define EXPECT_ERROR(expr) {                    \
+    bool error = false;                         \
+    try {                                       \
+      expr;                                     \
+    } catch (std::exception e) {                \
+      error = true;                             \
+    }                                           \
+    EXPECT_TRUE(error);                         \
+  }                                             \
+
+TEST(Constructor, InitializerList) {
   alnesjo::vector<int> u, v({1,2,3,4,5,6});
   u.push_back(1);
   u.push_back(2);
@@ -12,12 +22,26 @@ TEST(VectorTest, InitializerListConstructor) {
   EXPECT_TRUE(std::equal(u.begin(), u.end(), v.begin()));
 }
 
-TEST(VectorTest, SizeConstructor) {
+TEST(Constructor, EmptyInitializerList) {
+  alnesjo::vector<int> u, v({});
+  EXPECT_TRUE(std::equal(u.begin(), u.end(), v.begin()));
+}
+
+TEST(Constructor, PositiveSize) {
   alnesjo::vector<int> u({0,0,0,0,0,0}), v(6);
   EXPECT_TRUE(std::equal(u.begin(), u.end(), v.begin()));
 }
 
-TEST(VectorTest, CopyConstructor) {
+TEST(Constructor, ZeroSize) {
+  alnesjo::vector<int> u, v(0);
+  EXPECT_TRUE(std::equal(u.begin(), u.end(), v.begin()));
+}
+
+TEST(Constructor, NegativeSize) {
+  EXPECT_ERROR(alnesjo::vector<int> u(-6));
+}
+
+TEST(Constructor, Copy) {
   alnesjo::vector<int> u(10,7), v;
   v = u;
   EXPECT_NE(u.begin(), v.begin());
@@ -29,14 +53,14 @@ TEST(VectorTest, CopyConstructor) {
   }
 }
 
-TEST(VectorTest, MoveConstructor) {
+TEST(Constructor, Move) {
   alnesjo::vector<int> u(10,7), v;
   auto it = u.begin();
   v = std::move(u);
   EXPECT_EQ(it, v.begin());
 }
 
-TEST(VectorTest, PushBack) {
+TEST(PushBack, Natural) {
   alnesjo::vector<int> v;
   for (int i = 0; i < 10; i++) {
     v.push_back(i);
@@ -45,7 +69,7 @@ TEST(VectorTest, PushBack) {
   EXPECT_EQ(4, v[4]);
 }
 
-TEST(VectorTest, EmplaceBack) {
+TEST(EmplaceBack, Natural) {
   alnesjo::vector<std::tuple<int,int,int>> u({std::make_tuple(1,2,3),
                                               std::make_tuple(4,5,6)}), v;
   v.emplace_back(1,2,3);
@@ -54,7 +78,7 @@ TEST(VectorTest, EmplaceBack) {
 }
 
 
-TEST(VectorTest, InsertAtFront) {
+TEST(Insert, AtFront) {
   alnesjo::vector<int> v;
   for (int i = 0; i < 10; i++) {
     v.insert(0,i);
@@ -63,18 +87,18 @@ TEST(VectorTest, InsertAtFront) {
   EXPECT_EQ(5, v[4]);
 }
 
-TEST(VectorTest, EmplaceAtFront) {
-  alnesjo::vector<std::tuple<int,int>> u({std::make_tuple(1,2),
+TEST(Emplace, AtFront) {
+  alnesjo::vector<std::tuple<int,int>> u({std::make_tuple(5,6),
                                           std::make_tuple(3,4),
-                                          std::make_tuple(5,6)}), v;
-  v.emplace_back(1,2);
-  v.emplace_back(3,4);
-  v.emplace_back(5,6);
+                                          std::make_tuple(1,2)}), v;
+  v.emplace(0,1,2);
+  v.emplace(0,3,4);
+  v.emplace(0,5,6);
   EXPECT_TRUE(std::equal(u.begin(), u.end(), v.begin()));
 }
 
 
-TEST(VectorTest, EraseSingle) {
+TEST(Erase, Single) {
   alnesjo::vector<int> v({1,2,3,4,5,6});
   v.erase(2);
   EXPECT_EQ(1, v[0]);
@@ -83,7 +107,7 @@ TEST(VectorTest, EraseSingle) {
   EXPECT_EQ(5, v.size());
 }
 
-TEST(VectorTest, EraseRange) {
+TEST(Erase, Range) {
   alnesjo::vector<int> v({1,2,3,4,5,6});
   v.erase(v.begin(), v.end()-3);
   EXPECT_EQ(4, v[0]);
@@ -92,7 +116,7 @@ TEST(VectorTest, EraseRange) {
   EXPECT_EQ(3, v.size());
 }
 
-TEST(VectorTest, EraseRemove) {
+TEST(Erase, Remove) {
   alnesjo::vector<int> v({1,2,3,2,5,2});
   v.erase(std::remove(v.begin(), v.end(), 2), v.end());
   EXPECT_EQ(1, v[0]);
@@ -101,7 +125,7 @@ TEST(VectorTest, EraseRemove) {
   EXPECT_EQ(3, v.size());
 }
 
-TEST(VectorTest, Find) {
+TEST(Find, Natural) {
   alnesjo::vector<int> v({1,2,3,4,5,6});
   EXPECT_EQ(v.begin()+2, v.find(3));
   EXPECT_EQ(v.begin()+5, v.find(6));
